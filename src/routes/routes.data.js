4794e0acb277c54e4f4eb4c645de1165b1b17fd9
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-03-28 18:13:40 +0800

    Add
    functional new-workout

--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -1,5 +1,6 @@
 import Auth from '../components/screens/auth/Auth'
 import Home from '../components/screens/home/Home'
+import NewExercise from '../components/screens/new-exercise/NewExercise'
 import NewWorkout from '../components/screens/new-workout/NewWorkout'
 import Profile from '../components/screens/profile/Profile'
 
@@ -23,30 +24,30 @@ export const routes = [
     path: '/profile',
     component: Profile,
     isAuth: true
-  } /* 
-	{
-		path: '/new-exercise',
+  },
+  {
+    path: '/new-exercise',
 
-		component: NewExercise,
-		isAuth: true,
-	},
-
-	{
-		path: '/workout/:id',
-
-		component: SingleWorkout,
-		auth: true,
-	},
-	{
-		path: '/workouts',
-
-		component: ListWorkouts,
-		isAuth: true,
-	},
-	{
-		path: '/exercise/:id',
-
-		component: SingleExercise,
-		isAuth: true,
-	}, */
+    component: NewExercise,
+    isAuth: true,
+  },
+  /*
+    {
+      path: '/workout/:id',
+  
+      component: SingleWorkout,
+      auth: true,
+    },
+    {
+      path: '/workouts',
+  
+      component: ListWorkouts,
+      isAuth: true,
+    },
+    {
+      path: '/exercise/:id',
+  
+      component: SingleExercise,
+      isAuth: true,
+    }, */
 ]
