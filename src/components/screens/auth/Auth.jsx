621a62492888d0f634b11a4d75b552b4f4a550cb
Константин Андреевич Жуковский <Jukovsky.k.a@yandex.ru>
2023-04-01 23:25:06 +0800

    fixed
    Problems with server side queries have been fixed

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -13,7 +13,7 @@ const Auth = () => {
 
 	return (
 		<>
-			<Layout heading='Sign in' bgImage='/images/auth-bg.png' />
+			<Layout heading='Sign in' bgImage='/images/auth-bg.jpg' />
 			<div className='wrapper-inner-page'>
 				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
